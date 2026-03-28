1e4a3a11732c083bb66cce67aca3422954448922
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:45 +0700

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:41.813Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-28T09:03:44.664Z
+Commit: Add multi-language support
