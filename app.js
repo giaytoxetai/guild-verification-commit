f8846472e551eed8865b523d9e7b7ed95246a01c
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:44 +0700

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:40.427Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-28T09:03:43.224Z
+Commit: Fix broken links in documentation
