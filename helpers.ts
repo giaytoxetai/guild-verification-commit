fdce714e81b9b256b53c727962008cb255e9593c
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:32 +0700

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:23.515Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-28T09:03:31.262Z
+Commit: Fix broken links in documentation
