e1a8743829318890d0b8162daf6755eaf8934a0d
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:19 +0700

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:07.696Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-28T09:03:18.737Z
+Commit: Fix race condition in async operations
