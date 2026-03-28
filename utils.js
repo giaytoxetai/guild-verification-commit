08b31a873b2c16c7d32134db7f5963a063114876
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:47 +0700

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:44.664Z
-Commit: Add multi-language support
+Timestamp: 2026-03-28T09:03:46.643Z
+Commit: Patch security vulnerability in authentication
