912c81233064fd399842b58dafbad4c49157dff3
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:01:52 +0700

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:01:19.474Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-28T09:01:51.333Z
+Commit: Fix memory leak in event handlers
