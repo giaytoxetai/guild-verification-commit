540e8bca535899b2dafe640426af29b38495487e
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:02:50 +0700

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:02:45.722Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-28T09:02:49.180Z
+Commit: Introduce real-time notifications
