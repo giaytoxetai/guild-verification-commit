4aa2a5b000680079aff68f680adda7ef47248394
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:03:33 +0700

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:31.262Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-28T09:03:32.662Z
+Commit: Patch security vulnerability in authentication
