e78e92c1fa49baba50fbd1eafd33c03699449732
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:04:01 +0700

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:03:50.977Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-28T09:04:00.460Z
+Commit: Fix CSS styling bug on mobile devices
