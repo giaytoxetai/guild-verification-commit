006f907b61a9d37a27c6435300f965463ccd4633
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:02:47 +0700

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:02:42.347Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-28T09:02:45.722Z
+Commit: Resolve null pointer exception in user service
