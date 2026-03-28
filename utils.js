48bbef9dc50a25d45fa9b30ace009a4865649b31
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:01:58 +0700

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:01:46.746Z
-Commit: Add multi-language support
+Timestamp: 2026-03-28T09:01:57.338Z
+Commit: Resolve null pointer exception in user service
