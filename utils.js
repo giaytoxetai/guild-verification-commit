fe59ab82bda13ca8414208422241a95cba098ac3
giaytoxetai <giaytoxetai@gmail.com>
2026-03-28 16:02:43 +0700

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T09:02:38.704Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-28T09:02:42.347Z
+Commit: Fix CSS styling bug on mobile devices
